7140b2096ca1fc611dd44ab915a3c6284939651a
Rodrigo Carpio <rodrigo25096@gmail.com>
2024-11-05 08:10:18 -0500

    Add emoji day/night

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
+from datetime import datetime
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
+def obtener_saludo():
+    hora_actual = datetime.now().hour
+    if 6 <= hora_actual < 18:
+        return "🌞 Bueeeenaaa"
+    else:
+        return "🌚 Bueeeenaaa"
+
 async def bienvenida(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for nuevo in update.message.new_chat_members:
         username = f"@{nuevo.username}" if nuevo.username else nuevo.first_name
+        saludo = obtener_saludo()
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=f"Bueeena, {username}! 🦭🦭🦭"
+            text=f"{saludo}, {username}! 🦭🦭🦭"
         )
         try:
-            await context.bot.send_audio(
-                chat_id=update.effective_chat.id,
-                audio=open('oñooo.mp3', 'rb')
-            )
+            with open('porlas.mp3', 'rb') as audio_file:
+                await context.bot.send_audio(
+                    chat_id=update.effective_chat.id,
+                    audio=audio_file
+                )
         except Exception as e:
             print(f"Error al enviar audio: {e}")
 
@@ -23,4 +33,5 @@ application = ApplicationBuilder().token(TOKEN).build()
 application.add_handler(
     MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, bienvenida)
 )
+
 application.run_polling()
